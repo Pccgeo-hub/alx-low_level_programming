ff9772a059d680ef767575228291a46ad1ef2671
chinyere <pchukwu757@example.com>
2022-09-08 16:25:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: always 0
  */
-int main(void) 
+intmain(void) 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+return(0); 
 }

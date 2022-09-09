31e6a103d16f1d2569146e7bd4782bed40fc5abd
chinyere <pchukwu757@gmail.com>
2022-09-09 16:55:04 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)  
+int main(void)
 {
 int i;
 for (i = 122; i > 96; i--)
-{      
+{
 putchar(i);
 }
-putchar('\n');  
-return (0); 
+putchar('\n');
+return (0);
 }

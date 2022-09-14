eff445d0c160b49bd8567e8743cf1611f8e31523
chinyere <pchukwu757@gmail.com>
2022-09-14 15:50:04 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  *
  */
 
-void print_alphabet_x10(void) 
-{  
-char i;
-char h;  
-for (h = 0; h <= 9; h++)
-{      
-for (i = 'a'; i <= 'z'; i++)	
+void print_alphabet_x10(void)
 {
-_putchar(i);	  
-}      
-_putchar('\n');      
-} 
+char i;
+char h;
+for (h = 0; h <= 9; h++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
 

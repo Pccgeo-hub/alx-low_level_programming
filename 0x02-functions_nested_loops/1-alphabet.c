f23731d9daf941641db52515f1f20dcb857d608b
chinyere <pchukwu757@gmail.com>
2022-09-14 15:32:28 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,41 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_alphabet - check the code for ALX School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-
+ * print_alphabet - prints alphabet
+ *  
+ * Return: always 0;
+ * 
+ */
 
 void print_alphabet(void)
-
 {
-
-		char alp = 'a';
-
-
-
-			while (alp <= 'z')
-
-					{
-
-								_putchar (alp);
-
-										alp++;
-
-
-
-											}
-
-				_putchar ('\n');
-
-
-
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
 }
+_putchar('\n');
+}
+
